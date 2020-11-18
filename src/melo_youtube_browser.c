4b59d8b5c1d6e0bc0d69a23c2b99cfd10a7dbfe4
Alexandre Dilly <dillya@sparod.com>
2020-11-18 21:40:44 +0100

    browser: update protobuf actions API

--- src/melo_youtube_browser.c
+++ src/melo_youtube_browser.c
@@ -176,7 +176,7 @@ list_cb (MeloHttpClient *client, JsonNode *node, void *user_data)
   static Browser__SortMenu *sort_menus_ptr[1] = {
       &sort_menus,
   };
-  static Browser__Action actions[4] = {
+  static Browser__Action actions[] = {
       {
           .base = PROTOBUF_C_MESSAGE_INIT (&browser__action__descriptor),
           .type = BROWSER__ACTION__TYPE__PLAY,
@@ -202,16 +202,14 @@ list_cb (MeloHttpClient *client, JsonNode *node, void *user_data)
           .icon = "fa:star",
       },
   };
-  static Browser__Action *set_fav_actions_ptr[3] = {
+  static Browser__Action *actions_ptr[] = {
       &actions[0],
       &actions[1],
       &actions[2],
-  };
-  static Browser__Action *unset_fav_actions_ptr[3] = {
-      &actions[0],
-      &actions[1],
       &actions[3],
   };
+  static uint32_t set_fav_actions[] = {0, 1, 2};
+  static uint32_t unset_fav_actions[] = {0, 1, 3};
   MeloRequest *req = user_data;
 
   /* Make media list response from JSON node */
@@ -268,6 +266,10 @@ list_cb (MeloHttpClient *client, JsonNode *node, void *user_data)
     /* Set list count */
     media_list.count = count;
 
+    /* Set actions */
+    media_list.n_actions = G_N_ELEMENTS (actions_ptr);
+    media_list.actions = actions_ptr;
+
     /* Add media items */
     for (i = 0; i < count; i++) {
       JsonObject *o, *id, *snip;
@@ -295,17 +297,17 @@ list_cb (MeloHttpClient *client, JsonNode *node, void *user_data)
       items[i].name = (char *) json_object_get_string_member (snip, "title");
       items[i].type = BROWSER__RESPONSE__MEDIA_ITEM__TYPE__MEDIA;
 
-      /* Set favorite and actions */
+      /* Set favorite and action IDs */
       media_id = melo_library_get_media_id_from_browser (
           MELO_YOUTUBE_BROWSER_ID, items[i].id);
       items[i].favorite =
           melo_library_media_get_flags (media_id) & MELO_LIBRARY_FLAG_FAVORITE;
       if (items[i].favorite) {
-        items[i].n_actions = G_N_ELEMENTS (unset_fav_actions_ptr);
-        items[i].actions = unset_fav_actions_ptr;
+        items[i].n_action_ids = G_N_ELEMENTS (unset_fav_actions);
+        items[i].action_ids = unset_fav_actions;
       } else {
-        items[i].n_actions = G_N_ELEMENTS (set_fav_actions_ptr);
-        items[i].actions = set_fav_actions_ptr;
+        items[i].n_action_ids = G_N_ELEMENTS (set_fav_actions);
+        items[i].action_ids = set_fav_actions;
       }
 
       /* Set tags */
